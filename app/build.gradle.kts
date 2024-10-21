b8b0603c748d3a5138b9eccb98eb8ce1f728746e
Crhistian Ninalaya <133820344+CrhistianNinalaya@users.noreply.github.com>
2024-10-20 20:06:39 -0500

    añadida coneccion con firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.googleService)
+
 }
 
 android {
@@ -42,8 +44,17 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(kotlin("script-runtime"))
+
+//    Traido para implementar Firebase
+    implementation (platform("com.google.firebase:firebase-bom:32.1.0"))
+    implementation("com.google.firebase:firebase-firestore-ktx:25.1.0")
+    implementation("com.google.firebase:firebase-core:21.1.1")
+
+    //ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
 }
--- app/src/main/java/com/example/proyecto_aplicaciones_moviles_i/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_aplicaciones_moviles_i/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.proyecto_aplicaciones_moviles_i
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -18,5 +22,52 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+// Inicializa FirebaseAuth
+        val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+        // Función para registrar un nuevo usuario
+        fun registerUser(email: String, password: String) {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // Registro exitoso
+                        val user = auth.currentUser
+                        Log.d("FirebaseAuth", "Registro exitoso. Usuario: ${user?.email}")
+                    } else {
+                        // Si el registro falla, muestra un mensaje de error
+                        Log.w("FirebaseAuth", "Error al registrar usuario", task.exception)
+                    }
+                }
+        }
+
+        // Función para logearse un  usuario
+        fun logearse(email: String, password: String) {
+            val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // Inicio de sesión exitoso
+                        val user = auth.currentUser
+                        Log.d("FirebaseAuth", "Inicio de sesión exitoso. Usuario: ${user?.email}")
+                        Toast.makeText(this, "Inicio Exitoso: ", Toast.LENGTH_SHORT).show()
+                        // Puedes aquí redirigir al usuario a una nueva actividad o hacer alguna acción
+                    } else {
+                        // Si falla, mostrar mensaje de error
+                        Log.w("FirebaseAuth", "Error al iniciar sesión", task.exception)
+                        // Mostrar un Toast o mensaje al usuario
+                        Toast.makeText(
+                            this,
+                            "Error al iniciar sesión: ${task.exception?.message}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+
+// llamado de los metodos
+//registerUser("testuser@example.com", "password123")
+        logearse("testuser1@example.com", "password123")
+
     }
 }
